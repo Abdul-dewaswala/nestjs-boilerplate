17b543a487aefda96442ff4853258b27027ac0fc
Abdul Quadir Dewaswala <abdul.shantiinfotech@gmail.com>
2025-10-11 14:00:08 +0530

    update code

--- src/database/migrations/20251011064859-create-users-roles.js
+++ src/database/migrations/20251011064859-create-users-roles.js
@@ -3,6 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
+    await queryInterface.sequelize.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";');
     await queryInterface.createTable('users', {
       id: {
         type: Sequelize.UUID,
@@ -12,7 +13,8 @@ module.exports = {
       },
       auth0Id: {
         type: Sequelize.STRING,
-        allowNull: false,
+        allowNull: true,
+        defaultValue: null,
         unique: true,
       },
       email: {
@@ -23,14 +25,25 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: true,
       },
+      middleName: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        defaultValue: null,
+      },
       lastName: {
         type: Sequelize.STRING,
         allowNull: true,
+        defaultValue: null,
       },
       onboardingCompleted: {
         type: Sequelize.BOOLEAN,
         defaultValue: false,
         allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        defaultValue: null,
       },
       createdAt: {
         type: Sequelize.DATE,
@@ -47,12 +60,31 @@ module.exports = {
         type: Sequelize.UUID,
         defaultValue: Sequelize.UUIDV4,
         primaryKey: true,
-        allowNull: false,
+        allowNull: true,
       },
       name: {
         type: Sequelize.STRING,
         allowNull: false,
         unique: true,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+
+    await queryInterface.createTable('role_user', {
+      roleId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'roles',
+          key: 'id',
+        },
       },
       userId: {
         type: Sequelize.UUID,
@@ -61,8 +93,6 @@ module.exports = {
           model: 'users',
           key: 'id',
         },
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE',
       },
       createdAt: {
         type: Sequelize.DATE,
@@ -78,5 +108,7 @@ module.exports = {
   async down (queryInterface, Sequelize) {
     await queryInterface.dropTable('roles');
     await queryInterface.dropTable('users');
+    await queryInterface.dropTable('role_user');
+    await queryInterface.sequelize.query('DROP EXTENSION IF EXISTS "uuid-ossp";');
   }
 };
